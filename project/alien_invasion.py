c362f21916640aa938093d0c37cd18bb72541149
Jakob Wolitzki <jakob.wolitzki@gmail.com>
2019-01-27 20:55:02 +0100

    added ship

--- project/alien_invasion.py
+++ project/alien_invasion.py
@@ -2,12 +2,21 @@ import sys
 
 import pygame
 
+from settings import Settings
+from ship import Ship
 
 def run_game():
     # Initialize game and create a screemn obj.
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width,
+         ai_settings.screen_height)
+    )
     pygame.display.set_caption('Alien Invasion')
+
+    # Make ship
+    ship = Ship(screen)
 
     # Start the main loop for the game
     while True:
@@ -16,7 +25,12 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        # Redraw the screen during each pass through loop.
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
         # Make the most recently dawn screen visible.
         pygame.display.flip()
+
 
 run_game()
