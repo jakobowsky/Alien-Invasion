22eb116fa4a5122dfa525987a798b4ee5c1fec6d
Jakob Wolitzki <jakob.wolitzki@gmail.com>
2019-01-29 22:34:31 +0100

    end of 12 chapter

--- project/game_functions.py
+++ project/game_functions.py
@@ -12,11 +12,14 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        # Create new bullet and add it to the bullets group.
-        if len(bullets) < ai_settings.bullets_allowed:
-            new_bullet = Bullet(ai_settings, screen, ship)
-            bullets.add(new_bullet)
+        fire_bullet(ai_settings, screen, ship, bullets)    
 
+def fire_bullet(ai_settings, screen, ship, bullets):
+    ''' Fire a bullet if limit not reached yet '''
+    # Create new bullet and add it to the bullets group.
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
 def check_keyup_events(event, ship):
     if event.key == pygame.K_RIGHT:
@@ -40,6 +43,16 @@ def check_events(ai_settings, screen, ship, bullets):
             )
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
+
+def update_bullets(bullets):
+    ''' Update position of bullets and get rid of old bullets '''
+    # Update bullet positions.
+    bullets.update()
+    # Get rid of bullets that have disappeared.
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+                bullets.remove(bullet)
+    #print(len(bullets))
 
 
 def update_screen(ai_settings, screen, ship, bullets):
