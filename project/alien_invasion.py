a7ee229cff1b5461b1b4b5bb302499e003a3782d
Jakob Wolitzki <jakob.wolitzki@gmail.com>
2019-01-29 22:14:26 +0100

    added bullets

--- project/alien_invasion.py
+++ project/alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
@@ -20,13 +21,15 @@ def run_game():
     # Make ship
     ship = Ship(ai_settings, screen)
 
+    # Make a group to store bulleets in.
+    bullets = Group()
+
     # Start the main loop for the game
     while True:
-
-        # Watch for keyboard and mouse events.
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()
---
+++ project/bullet.py
@@ -0,0 +1,33 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Bullet(Sprite):
+    ''' A class to manage bullets fired from the ship '''
+
+    def __init__(self, ai_settings, screen, ship):
+        ''' Create a bullet object at the ships current position '''
+        super.__init__()
+        self.screen = screen
+
+        # Create a bullet rect at (0, 0) and then set correct position.
+        self.rect = pygame.Rect(0, 0, ai_settings.bullet_width,
+                                ai_settings.bullet_height)
+        self.rect.centerx = ship.rect.centerx
+        self.rect.top = ship.rect.top
+
+        # Store the bullet' s position as a decimal value.
+        self.y = float(self.rect.y)
+
+        self.color = ai_settings.bullet_color
+        self.speed_factor = ai_settings.bullet_speed_factor
+
+    def update(self):
+        ''' Move the bullet up the screen '''
+        # update the decimal posititon of the bullet
+        self.y -= self.speed_factor
+        # update the rect_pos
+        self.rect.y = self.y
+
+    def draw_bullets(self):
+        pygame.draw.rect(self.screen, self.color, self.rect)
