a98ad5012a96456f30d3e59bf0b13bebf1dce230
Jakob Wolitzki <jakob.wolitzki@gmail.com>
2019-01-31 23:21:57 +0100

    button

--- project/game_stats.py
+++ project/game_stats.py
@@ -5,7 +5,9 @@ class GameStats():
         ''' Initialize statistics '''
         self.ai_settings = ai_settings
         self.reset_stats()
-        self.game_active = True
+
+        # Start game in an inactive state .
+        self.game_active = False
 
     def reset_stats(self):
         ''' Initialize staatistics than can change during the game '''
