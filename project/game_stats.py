d8352b0bfe6ce10a209d8a578c79bfbc37f407b7
Jakob Wolitzki <jakob.wolitzki@gmail.com>
2019-02-03 19:26:20 +0100

    score

--- project/game_stats.py
+++ project/game_stats.py
@@ -12,3 +12,4 @@ class GameStats():
     def reset_stats(self):
         ''' Initialize staatistics than can change during the game '''
         self.ship_left = self.ai_settings.ship_limit
+        self.score = 0
